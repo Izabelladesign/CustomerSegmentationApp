bdac3d840234a0791823b90f83b2e39ce718deda
Zara Rahim <zararahim1205@gmail.com>
2025-12-04 23:50:42 -0800

    Update DBConnection.java

--- app/src/db/DBConnection.java
+++ app/src/db/DBConnection.java
@@ -7,6 +7,7 @@ import java.io.InputStream;
 
 public class DBConnection {
 
+    //Loads database configuration properties from the db.properties file
     private static Properties loadProps() throws Exception {
         Properties props = new Properties();
         try (InputStream in = DBConnection.class.getResourceAsStream("/db/db.properties")) {
@@ -17,7 +18,7 @@ public class DBConnection {
         }
         return props;
     }
-
+//creates and returns a database connection using credentials
     public static Connection get() throws Exception {
         Properties props = loadProps();
         String url = props.getProperty("url");
